51bdd0b50a3e677930dab4462a513f17fbc9ee94
narah10 <kwa18001@byui.edu>
2023-10-28 16:05:19 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Overview
+
+I'm a web developer with a passion for learning and improving. I built this contacts web app to stay updated on the latest web technologies and best practices with REACT, Node.js and MongoDB. My goal is to create more user-friendly web applications as I continue to grow in the field of software engineering.
+
+I developed a Contacts React Web App that displays information from an API that I was able to create with Node.js and using the MongoDB framework as a demonstration of my proficiency in the Javascript language in web development. This web app is able to display the contacts information to users, as well as the ability to create, edit and delete the contacts. 
+
+{Describe your purpose for writing this software.}
+To run the web app locally, here are the steps to take in the terminal:
+* `git clone `
+* `cd backend`
+* Run 'node server.js' so the API server can start running and be able to connect with the front-end
+* Run `cd ..` to return to the contacts directory and then run `cd contacts-web-app`
+* Run `npm install`
+* Run `npm start` 
+* Open http://localhost:3000 to view it in your browser.
+
+{Provide a link to your YouTube demonstration.  It should be a 4-5 minute demo of the software running (starting the server and navigating through the web pages) and a walkthrough of the code.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Web Pages
+
+This web app's main page displays the contacts in the database and does not have multiple web pages. It is focused more on using the created API and being able to change the information displayed as it also updates the database. 
+
+# Development Environment
+
+{Describe the tools that you used to develop the software}
+
+* Visual Studio Code
+* Git
+* MongoDB
+
+Here are the programming languages I used for this software
+
+* JavaScript
+* Node.js
+* CSS
+* REACT
+* Express.js
+
+# Useful Websites
+* [mongodb.com](https://www.mongodb.com/atlas/database)
+* [codeforgeek.com](https://codeforgeek.com/express-nodejs-tutorial/)
+
+# Future Work
+
+* Currently I can only change the database from using a rest client so I want to incorporate the frontend to have a method of adding information so the user's input can be sent to update the database
+* I want to make the web app more user friendly by improving the design
+*
---
+++ backend/controllers/contacts.js
@@ -0,0 +1,78 @@
+// const { response } = require('express');
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
+
+  const getAll = async (req, res) => {
+    const result = await mongodb.getDb().db('contacts').collection('contacts').find();
+    result.toArray().then((lists) => {
+      console.log(lists)
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists); 
+    });
+  };
+
+  const getSingle = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getDb()
+      .db('contacts')
+      .collection('contacts')
+      .find({ _id: userId });
+    result.toArray().then((lists) => {
+      console.log(lists)
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]);
+    });
+  };
+  
+//creating contact 
+const createContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  }
+  const result = await mongodb.getDb().db('contacts').collection('contacts').insertOne(contact);
+  console.log(result)
+  if (result.acknowledged){
+    res.status(201).json(result);
+  } else {
+    res.status(500).json(result.error);
+  }
+}
+
+//updating the contact
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  }
+  const result = await mongodb.getDb().db('contacts').collection('contacts').replaceOne({ _id: userId }, contact);
+  if (result.acknowledged){
+    res.status(204).json(result);
+  } else {
+    res.status(500).json(result.error);
+  }
+
+}
+
+
+//deleting contact by id 
+const deleteContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().db('contacts').collection('contacts').deleteOne({ _id: userId }, true);
+  if (result.acknowledged){
+    res.status(200).send();
+  } else {
+    res.status(500).json(result.error);
+  }
+
+}
+
+  module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
